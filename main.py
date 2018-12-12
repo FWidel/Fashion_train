22ada4197d7eba8fff7188af29ab5151c3e20394
Franciszek Wideł <frozenik12@gmail.com>
2018-12-12 16:42:56 +0100

    Update main.py
    fixed code

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pickle
- 
+
 item_train_data = []
 item_test_data = []
-(train_x_data, train_y_data), (test_x_data, test_y_data) = fashion_mnist.load_data()
+(train_x_data, train_y_data), (test_x_data,
+                               test_y_data) = fashion_mnist.load_data()
 for item_train in train_x_data:
     item_train_data.append(item_train.flatten())
 for item_test in test_x_data:
@@ -30,7 +31,7 @@ sns.heatmap(conf_matrix)
 print('The output score is: %s' %
       item_mnist_classifier.score(item_test_data, test_y_data))
 plt.show()
- 
+
 item_mnist_classifier = LogisticRegression(
     verbose=1, max_iter=6, multi_class="multinomial", solver="sag")
 item_mnist_classifier.fit(item_train_data, train_y_data)
@@ -47,12 +48,12 @@ item_mnist_classifier__from_file = pickle.load(
     open('item_mnist_classifier.model', 'rb'))
 conf_matrix = confusion_matrix(
     test_y_data, item_mnist_classifier__from_file.predict(item_test_data))
- 
+
 sns.heatmap(conf_matrix)
 print('The output score is:  %s' %
       item_mnist_classifier__from_file.score(item_test_data, test_y_data))
 plt.show()
- 
+
 img = image.load_img('file.png', target_size=(
     28, 28), grayscale=True, color_mode="grayscale")
 x = image.img_to_array(img)
